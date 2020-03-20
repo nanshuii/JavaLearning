65fb7c36a6f158c28a3df3fe1e352d6881679934
Ledon <ledonwxsp@163.com>
2020-03-20 10:05:46 +0800

    对象的创建和调用

---
+++ com/test/demo/test001.java
@@ -0,0 +1,10 @@
+package test;
+import test.HelloWorld;
+import com.sun.tools.javac.Main;
+
+public class test001 {
+    public static void main(String[] args) {
+        HelloWorld hl = new HelloWorld();
+        hl.test001();
+    }
+}
