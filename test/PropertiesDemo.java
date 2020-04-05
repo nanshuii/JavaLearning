c9becbb83410e71d4e464ebee86efbc2e5ff18ee
Ledon <ledonwxsp@163.com>
2020-04-05 16:42:16 +0800

    Properties

---
+++ test/PropertiesDemo.java
@@ -0,0 +1,20 @@
+package test;
+
+/*
+* Properties属性集
+* Properties extends Hashtable<k, v> implements Map<k, v>
+* 一个持久的属性集，可保存在流中或从流中加载；属性列表中每个键及其对应值都是一个字符串；
+*
+* 唯一和IO流相结合的集合
+* 可以使用Properties中的store，将集合中的临时数据持久化写入到硬盘中存储；
+* 可以使用Properties中的load，将硬盘中保存的文件读取到集合中使用
+*
+*
+*
+* */
+
+public class PropertiesDemo {
+    public static void main(String[] args) {
+
+    }
+}
