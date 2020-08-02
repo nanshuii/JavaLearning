9c3252da2ad56237f3df621f279a27017803e2f3
commit <ledonwxsp@163.com>
2020-08-02 18:20:25 +0800

    commit

---
+++ TomcatDemo/demo4/src/cn/servlet/ServletDemo.java
@@ -0,0 +1,33 @@
+package cn.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns="/demo")
+public class ServletDemo implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ TomcatDemo/demo5/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ledon
+  Date: 2020/8/2
+  Time: 4:59 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
