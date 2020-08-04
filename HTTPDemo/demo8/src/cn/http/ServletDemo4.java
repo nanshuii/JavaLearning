a49452a2d49be1d95c28c04e1bcb7622e910e517
commit <ledonwxsp@163.com>
2020-08-04 16:00:31 +0800

    response

---
+++ HTTPDemo/demo8/src/cn/http/ServletDemo4.java
@@ -0,0 +1,24 @@
+package cn.http;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/demo4")
+public class ServletDemo4 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        ServletOutputStream servletOutputStream = resp.getOutputStream();
+        servletOutputStream.write("你是臭傻逼".getBytes());
+    }
+}
