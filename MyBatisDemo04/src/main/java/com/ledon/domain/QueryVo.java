8742e391f9102c11380567c77c41df247c047927
commit <ledonwxsp@163.com>
2020-08-31 08:18:30 +0800

    typeAliases和package

---
+++ MyBatisDemo04/src/main/java/com/ledon/domain/QueryVo.java
@@ -0,0 +1,13 @@
+package com.ledon.domain;
+
+public class QueryVo {
+    private USER user;
+
+    public USER getUser() {
+        return user;
+    }
+
+    public void setUser(USER user) {
+        this.user = user;
+    }
+}
